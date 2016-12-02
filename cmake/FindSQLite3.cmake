7a76939e394afc8de2ada2c337943c3539860b08
louiz’ <louiz@louiz.org>
2016-12-02 14:31:43 +0100

    Fix the way we find the sqlite3 library

--- cmake/FindSQLite3.cmake
+++ cmake/FindSQLite3.cmake
@@ -1,81 +1,41 @@
-# - Try to find sqlite3 
-# Find sqlite3 headers, libraries and the answer to all questions.
+# - Find SQLite3
+# Find the SQLite3 library
 #
-#  SQLITE3_FOUND               True if sqlite3 got found
-#  SQLITE3_INCLUDEDIR          Location of sqlite3 headers 
-#  SQLITE3_LIBRARIES           List of libaries to use sqlite3
-#  SQLITE3_DEFINITIONS         Definitions to compile sqlite3 
+# This module defines the following variables:
+#   SQLITE3_FOUND  -  True if library and include directory are found
+# If set to TRUE, the following are also defined:
+#   SQLITE3_INCLUDE_DIRS  -  The directory where to find the header file
+#   SQLITE3_LIBRARIES  -  Where to find the library file
+#   SQLITE3_GENERATE_CPP - A function, to be used like this:
 #
-# Copyright (c) 2007 Juha Tuomala <tuju@iki.fi>
-# Copyright (c) 2007 Daniel Gollub <gollub@b1-systems.de>
-# Copyright (c) 2007 Alban Browaeys <prahal@yahoo.com>
+# For conveniance, these variables are also set. They have the same values
+# than the variables above.  The user can thus choose his/her prefered way
+# to write them.
+#   SQLITE3_INCLUDE_DIR
+#   SQLITE3_LIBRARY
 #
-#  Redistribution and use is allowed according to the terms of the New
-#  BSD license.
-#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
+# This file is in the public domain
 
-INCLUDE( FindPkgConfig )
-# Take care about sqlite3.pc settings
-IF ( Sqlite3_FIND_REQUIRED )
-  SET( _pkgconfig_REQUIRED "REQUIRED" )
-ELSE ( Sqlite3_FIND_REQUIRED )
-  SET( _pkgconfig_REQUIRED "" )
-ENDIF ( Sqlite3_FIND_REQUIRED )
+include(FindPkgConfig)
+pkg_check_modules(SQLITE3 sqlite3)
 
-IF ( SQLITE3_MIN_VERSION )
-	PKG_SEARCH_MODULE( SQLITE3 ${_pkgconfig_REQUIRED} sqlite3>=${SQLITE3_MIN_VERSION} )
-ELSE ( SQLITE3_MIN_VERSION )
-	pkg_search_module( SQLITE3 ${_pkgconfig_REQUIRED} sqlite3 )
-ENDIF ( SQLITE3_MIN_VERSION )
+if(NOT SQLITE3_FOUND)
+  find_path(SQLITE3_INCLUDE_DIRS NAMES sqlite3.h
+    DOC "The SQLite3 include directory")
 
+  find_library(SQLITE3_LIBRARIES NAMES sqlite3
+    DOC "The SQLite3 library")
 
-# Look for sqlite3 include dir and libraries w/o pkgconfig
-IF ( NOT SQLITE3_FOUND AND NOT PKG_CONFIG_FOUND )
-	FIND_PATH( _sqlite3_include_DIR sqlite3.h 
-		PATHS
-		/opt/local/include/
-		/sw/include/
-		/usr/local/include/
-		/usr/include/
-	)
-	FIND_LIBRARY( _sqlite3_link_DIR sqlite3 
-		PATHS
-		/opt/local/lib
-		/sw/lib
-		/usr/lib
-		/usr/local/lib
-		/usr/lib64
-		/usr/local/lib64
-		/opt/lib64
-	)
-	IF ( _sqlite3_include_DIR AND _sqlite3_link_DIR )
-		SET ( _sqlite3_FOUND TRUE )
-	ENDIF ( _sqlite3_include_DIR AND _sqlite3_link_DIR )
+  # Use some standard module to handle the QUIETLY and REQUIRED arguments, and
+  # set SQLITE3_FOUND to TRUE if these two variables are set.
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(SQLITE3 REQUIRED_VARS SQLITE3_LIBRARIES SQLITE3_INCLUDE_DIRS)
 
+  # Compatibility for all the ways of writing these variables
+  if(SQLITE3_FOUND)
+    set(SQLITE3_INCLUDE_DIR ${SQLITE3_INCLUDE_DIRS})
+    set(SQLITE3_LIBRARY ${SQLITE3_LIBRARIES})
+  endif()
+endif()
 
-	IF ( _sqlite3_FOUND )
-		SET ( SQLITE3_INCLUDE_DIRS ${_sqlite3_include_DIR} )
-		SET ( SQLITE3_LIBRARIES ${_sqlite3_link_DIR} )
-	ENDIF ( _sqlite3_FOUND )
-
-	# Report results
-	IF ( SQLITE3_LIBRARIES AND SQLITE3_INCLUDE_DIRS AND _sqlite3_FOUND )	
-		SET( SQLITE3_FOUND 1 )
-		IF ( NOT Sqlite3_FIND_QUIETLY )
-			MESSAGE( STATUS "Found sqlite3: ${SQLITE3_LIBRARIES} ${SQLITE3_INCLUDE_DIRS}" )
-		ENDIF ( NOT Sqlite3_FIND_QUIETLY )
-	ELSE ( SQLITE3_LIBRARIES AND SQLITE3_INCLUDE_DIRS AND _sqlite3_FOUND )	
-		IF ( Sqlite3_FIND_REQUIRED )
-			MESSAGE( SEND_ERROR "Could NOT find sqlite3" )
-		ELSE ( Sqlite3_FIND_REQUIRED )
-			IF ( NOT Sqlite3_FIND_QUIETLY )
-				MESSAGE( STATUS "Could NOT find sqlite3" )	
-			ENDIF ( NOT Sqlite3_FIND_QUIETLY )
-		ENDIF ( Sqlite3_FIND_REQUIRED )
-	ENDIF ( SQLITE3_LIBRARIES AND SQLITE3_INCLUDE_DIRS AND _sqlite3_FOUND )	
-
-ENDIF ( NOT SQLITE3_FOUND AND NOT PKG_CONFIG_FOUND )
-
-# Hide advanced variables from CMake GUIs
-MARK_AS_ADVANCED( SQLITE3_LIBRARIES SQLITE3_INCLUDE_DIRS )
+mark_as_advanced(SQLITE3_INCLUDE_DIRS SQLITE3_LIBRARIES)
